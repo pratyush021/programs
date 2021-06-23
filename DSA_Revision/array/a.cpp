3fe5e20f0f945593f6a41f35c9b47308c7962c64
pratyush021 <probohidar2016@gmail.com>
2021-06-23 10:25:35 +0530

    late commit

--- DSA_Revision/array/a.cpp
+++ DSA_Revision/array/a.cpp
@@ -1,11 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+# define MAX 10000
+void test_case(){ 
+	int x, y, i, j;
+	cin >> x >> y >> i >> j;
+	if(i == 1) {
+		i = j;
+		j = 1;
+	} else if(j == 1) {
+		j = i;
+		i = 1;
+	} else {
+		
+	}
+}
 int main() {
 	int n;
 	cin >> n;
-	cout << n << endl;
-	return 0;
+	for(int i = 0; i < n; i++) {
+		test_case();
+	}
 }
 
 //lol
---
+++ a.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.Arrays;
+class a{
+	public static void main(String []args) {
+		int a, b, c;
+		int arr[] = new int[3];
+		Scanner sc = new Scanner(System.in);
+		arr[0] = sc.nextInt();
+		arr[1] = sc.nextInt();
+		arr[2] = sc.nextInt();
+		1+2*3=7
+1*(2+3)=5
+1*2*3=6
+(1+2)*3=9
+		int temp = 
+		System.out.println(sol);
+	}
+}
