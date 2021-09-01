ae4a21ca6898d8d0a7a55f715da3c0d3bb386858
pratyush021 <probohidar2016@gmail.com>
2021-09-01 10:45:59 +0530

    oop

--- a.cpp
+++ a.cpp
@@ -2,84 +2,23 @@
 using namespace std;
 #define MAX 100000
 #define ll long long
-static string s;
-bool check(string a, string s) {
-  for(int i =0; i < s.size(); i++) {
-    if(s[i] == a[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-void rotate(string &s)
-{
-    reverse(s.begin(), s.begin()+1);
-    reverse(s.begin()+1, s.end());
-    reverse(s.begin(), s.end());
-}
-vector<string> anagram(string input){
-  std::vector<string> ss;
-    sort(input.begin(), input.end());
-    do
-    // if(s[0] != input[0] && s[s.size()-1] != input[s.size()-1]) {
-    if(check(input, s)==true) {
-    
-        ss.push_back(input);
-      }
-    while(next_permutation(input.begin(), input.end()));
-    return ss;
-}
-
 void test_case() {
-  // string s;
-  bool flag = true, f = false;
-  cin >> s;
-  string a = s;
-  // bool flag = true,  f= true;
-  // string a = s;
-  // for(int i=0; i < s.size(); i++) {
-  //   rotate(s);
-  //   for(int k = 0; k < s.size(); k++) {
-  //       if(a[k] == s[k]) {
-  //         flag = false;
-  //       }
-  //   }
-  //   if(flag == true) {
-  //       f = false;
-  //       // reverse(s.begin(), s.end());
-  //       cout << s << endl;
-  //       return;
-  //
-  //   }
-  //
-  // }
-  // if(f = true) {
-  //   cout << "IMPOSSIBLE" << endl;
-  // }
-  std::vector<string> sol;
-  sol = anagram(a);
-  // cout << sol.size() << endl;
-  for(auto a: sol) {
-    if(check(a, s)==true) {
-      f = true;
-      cout << a << endl;
-      return;
-    }
-  }
-  if(f == false) {
-    cout << "IMPOSSIBLE" << endl;
-  }
+  ll a, b;
+  cin >> a >> b;
+  int res, res2;
+  res = b % a;
+  res2 = b % (a+1);
+  res = max(res, res2);
+  cout << res << endl;
 }
 
 int main(){
 	int t;
 	cin >> t;
 	for(int i = 0; i < t; i++) {
-		cout << "Case #"<<i+1<<": ";
+		// cout << "Case #"<<i+1<<": ";
 		test_case();
-		// cout << endl;
-		// cout << test_case() << endl;
-		// cout << debug() << endl;
+
 	}
 
 	return 0;
---
+++ oop/static.cpp
@@ -0,0 +1,11 @@
+#include<bits/stc++.h>
+using namespace std; 
+int demo() {
+    static int count = 0; 
+
+    cout << count << " ";
+}
+int main() { 	
+	
+	return 0;
+}
